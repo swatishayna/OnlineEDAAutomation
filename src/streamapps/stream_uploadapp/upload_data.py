78d388888ba8a93107f4b1a4b0e426769a583b31
Swati <swati308mmu@gmail.com>
2021-10-18 15:33:41 +0530

    project structure modified

--- src/streamapps/stream_uploadapp/upload_data.py
+++ src/streamapps/stream_uploadapp/upload_data.py
@@ -1,3 +1,4 @@
 import streamlit as st
 def app():
     st.write('Yes')
+   
--- src/streamapps/upload.py
+++ src/streamapps/upload.py
@@ -1,3 +1,15 @@
+# We can delete delete this
+# do not work in this because omkar said we dont need multipageapp for upload_data.... check app.py and stream_uploadapp
+
+
+
+
+
+
+
+
+
+
 # Custom imports
 from src.streamapps.multipage import MultiPage
 from src.streamapps.stream_uploadapp import upload_data
