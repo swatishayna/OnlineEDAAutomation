425792e9948f8318864ea99ee90448667313bf35
surajaiswal13 <surajaiswal13@gmail.com>
2021-10-22 22:55:34 +0530

    BasicEda part 1

--- src/utils/basic_def.py
+++ src/utils/basic_def.py
@@ -1,34 +1,45 @@
-import streamlit as st
 import pandas as pd
-from src.utils.basic_def import Basic
-from pathlib import Path
 import os
+from pathlib import Path
 
-def app():
-    basic = Basic()    
-    st.header("Basic Exploratory Data Analysis")
-    dataframe = basic.get_data("winequality_red.csv")
+class Basic:
+    def get_data(self, filename):
+        data_directory_path = os.path.join((Path(__file__).resolve().parent.parent),'data')
+        file_path = os.path.join(data_directory_path,filename)
+        return pd.read_csv(file_path)
 
-    column_names = st.checkbox("Get Column names of the dataframe")
-    if column_names:
-        st.write(basic.get_columns_names(dataframe))
-    
-    describe = st.checkbox("Get small description of the dataframe")
-    if describe:
-        st.write(basic.describe_dataset(dataframe))
-    
-    datatype = st.checkbox("Get Datatype list with respect to columns names of the dataframe")
-    if datatype:
-        st.write(basic.get_datatype_list(dataframe))
+    def get_shape(self,data):
+        return data.shape
 
-    duplicate_rows = st.checkbox("Get Duplicate rows of the datatframe")
-    if duplicate_rows:
-        st.write(basic.get_duplicate_rows(dataframe))
+    def get_missing_value(self):
+        return self.data.isnull()
 
-    view_df = st.checkbox("View Dataframe")
-    if view_df:
-        st.dataframe(dataframe)
+    def get_count_missing_value(self):
+        return self.data.isnull().sum()
 
-    datashape = st.checkbox("Get shape of the dataframe")
-    if datashape:
-        st.write(basic.get_shape(dataframe))
+    def get_percentage_missing_values(self, missing_value_count):
+        return missing_value_count/len(self.data)
+
+    # This Function  is used to get column names
+    def get_columns_names(self, df):
+        column = pd.DataFrame(df.columns)
+        return column
+
+    # This function is to get a small description of dataset
+    def describe_dataset(self, df):
+        describe = df.describe()
+        return describe
+
+    # This function is used to get datatypes of the columns
+    def get_datatype_list(self, df):
+        a = [i for i in df.dtypes]
+        b = [i for i in df.columns]
+        c = [str(i) for i in a]
+        datatype_list = {b[i]:c[i] for i in range(len(a))}
+        return pd.Series(datatype_list)
+
+    # This function is used to get Duplicate rows in a dataset
+    def get_duplicate_rows(self, df):
+        Duplicates = df[df.duplicated()]
+        return Duplicates
+
