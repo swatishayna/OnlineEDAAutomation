3838a0c7122e08ba40818d6e658dd286dcce8e2d
dyadav050 <dyadav050@gmail.com>
2021-10-27 19:05:45 +0530

    EDA Basics

--- src/streamapps/stream_basicapp/basic_info.py
+++ src/streamapps/stream_basicapp/basic_info.py
@@ -10,9 +10,23 @@ def app():
     dataframe = basic.get_data("winequality_red.csv")
     view_df = st.checkbox("View Dataframe")
     if view_df:
+        st.write("Dataframe")
         st.dataframe(dataframe)
 
     datashape = st.checkbox("Get shape of the dataframe")
     if datashape:
-        st.write(basic.get_shape(dataframe))
-        
+        st.write("Shape of the Dataframe:",basic.get_shape(dataframe))
+        st.write("Total number of Rows:", basic.get_shape(dataframe)[0])
+        st.write("Total number of Columns:", basic.get_shape(dataframe)[1])
+    
+    missing_values = st.checkbox("Get missing values")
+    if missing_values:
+        st.write("Missing values", basic.get_missing_value(dataframe))
+
+    missing_values_count = st.checkbox("Get count of missing values")
+    if missing_values_count:
+        st.write("Count of missing values", basic.get_count_missing_value(dataframe))
+
+    percentage_missing_values = st.checkbox("Get percentage of missing values")
+    if percentage_missing_values:
+        st.write("Percentage of missing value", basic.get_percentage_missing_values(dataframe, basic.get_count_missing_value(dataframe)))
--- src/utils/basic_def.py
+++ src/utils/basic_def.py
@@ -6,16 +6,17 @@ class Basic:
     def get_data(self, filename):
         data_directory_path = os.path.join((Path(__file__).resolve().parent.parent),'data')
         file_path = os.path.join(data_directory_path,filename)
+        # return pd.read_csv(file_path, delimiter=";")
         return pd.read_csv(file_path)
-
+        
     def get_shape(self,data):
         return data.shape
 
-    def get_missing_value(self):
-        return self.data.isnull()
+    def get_missing_value(self,data):
+        return data.isnull()
 
-    def get_count_missing_value(self):
-        return self.data.isnull().sum()
+    def get_count_missing_value(self, data):
+        return data.isnull().sum()
 
-    def get_percentage_missing_values(self, missing_value_count):
-        return missing_value_count/len(self.data)
+    def get_percentage_missing_values(self, data, missing_value_count):
+        return missing_value_count/len(data)
