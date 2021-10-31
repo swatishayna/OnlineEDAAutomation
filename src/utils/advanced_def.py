871b08afbedca24707dc97c23b6563d9aeb48ae0
Swati <swati308mmu@gmail.com>
2021-10-31 10:49:22 +0530

    correlation done

--- src/utils/advanced_def.py
+++ src/utils/advanced_def.py
@@ -2,17 +2,18 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import os
-from src.utils import basic_def
+
 
 class Advancedanalysis:
-    
+    def __init__(self,data):
+        self.data = data
 
 
 
     
      
     def generate_matrix_graph(self,chosen_method, graph_matrix):
-
+        
         if graph_matrix == 'Matrix':
             try:
                 matrix = self.data.corr(method=chosen_method)
@@ -25,8 +26,13 @@ class Advancedanalysis:
             return fig
 
     def generate_label_correlation(self,chosen_method,label):
+        try:
+            matrix = self.data.corr(method=chosen_method)[label]
+            write =  self.data.corr(method=chosen_method)[[label]].sort_values(by=label, ascending=False)
+            fig = px.imshow(write)
+            return matrix,fig
+        except:
+            return "Correlation Result cant be produced due to data insufficiency"
 
-        matrix = self.data.corr(method=chosen_method)[label]
-        write =  self.data.corr(method=chosen_method)[[label]].sort_values(by=label, ascending=False)
-        fig = px.imshow(write)
-        return matrix,fig
+
+   
