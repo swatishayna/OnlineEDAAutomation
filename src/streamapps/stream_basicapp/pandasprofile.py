c6cdb3b3ee4f93cb2c49c3f79b6b4a0b3764c445
Swati <swati308mmu@gmail.com>
2021-10-31 14:59:29 +0530

    pandas profile report added

--- src/streamapps/stream_basicapp/pandasprofile.py
+++ src/streamapps/stream_basicapp/pandasprofile.py
@@ -1,3 +1,19 @@
 import streamlit as st
+from src.utils.basic_def import Basic
+import numpy as np
+import pandas as pd
+import os
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+
+
+    
 def app():
-    st.write('Yes')
+    basic = Basic()    
+    st.header("Basic Exploratory Data Analysis")
+    dataframe = basic.get_data("train.csv")
+    pr = ProfileReport(dataframe, explorative=True, minimal=True,correlations={"cramers": {"calculate": False}})
+    
+    st.header('*Exploratory Data Analysis Fast Report*')
+    st_profile_report(pr)
